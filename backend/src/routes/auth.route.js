fd634f9b4def26f3518e5170550de20b829a0835
bhaleraorushi1213 <bhaleraorushi1213@gmail.com>
2026-07-06 18:51:06 +0530

    backend admin and songs controller implemented

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,7 +1,5 @@
 import { Router } from 'express';
-import { authCallback } from '../controller/auth.controller.js';
-
-import User from '../models/user.model.js';
+import { authCallback } from '../controller/auth.controller.js'
 
 const router = Router();
 
